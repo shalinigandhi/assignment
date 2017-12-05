e0b08c63e054cbe195e387558470ad49db3f42bf
Shalini Gandhi <shalinigandhi2@gmail.com>
2017-12-05 23:45:47 +0530

    Add json

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function fetchJSONFile(path, callback) {
+    debugger;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('js/data.json', function(data){
+    // do something with your data
+    debugger;
+    console.log(data);
+});
