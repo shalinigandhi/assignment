96cd098c71c0a2d66ad73f1099a458b763788c88
Shalini Gandhi <shalinigandhi2@gmail.com>
2017-12-06 11:22:36 +0530

    Retrieve data in json using pure javascript, Add functionality to buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var viewElement = document.querySelector('.view');
+
 function fetchJSONFile(path, callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
@@ -9,12 +11,67 @@ function fetchJSONFile(path, callback) {
         }
     };
     httpRequest.open('GET', path);
-    httpRequest.send(); 
+    httpRequest.send();
 }
 
-// this requests the file and executes a callback with the parsed result once
-//   it is available
-fetchJSONFile('js/data.json', function(data){
-    // do something with your data
-    console.log(data);
+function addLayout(layout) {
+    
+    if(layout=='grid') {
+        viewElement.classList.add('grid-view');
+        viewElement.classList.remove('list-view');
+    }
+    else if(layout=='list') {
+       viewElement.classList.add('list-view');
+       viewElement.classList.remove('grid-view');
+    }
+}
+
+fetchJSONFile('js/data.json', function(data) {
+
+    var blocksElement = document.querySelector('.blocks');
+
+    function createElements(car) {
+        var blockElement = document.createElement('div');
+        var imageElement = document.createElement('div');
+        var image = document.createElement('img');
+        var contentElement = document.createElement('div');
+        var titleElement = document.createElement('p');
+        var dateElement = document.createElement('span');
+        var descElement = document.createElement('p');
+
+        blockElement.appendChild(imageElement);
+        blockElement.appendChild(contentElement);
+
+        imageElement.appendChild(image);
+
+        contentElement.appendChild(titleElement);
+        contentElement.appendChild(dateElement);
+        contentElement.appendChild(descElement);
+
+        blockElement.className = 'block';
+        imageElement.className = 'image';
+        contentElement.className = 'content';
+        titleElement.className = 'title';
+        dateElement.className = 'date';
+        descElement.className = 'desc';
+
+        image.src = car.image;
+        titleElement.innerHTML = car.title;
+        dateElement.innerHTML = car.date;
+        descElement.innerHTML = car.desc;
+
+        return blockElement;
+    }
+
+    function addContent() {
+
+        for (var i = 0; i < data.cars.length; i++) {
+            var car = data.cars[i];
+            var card = createElements(car);
+            blocksElement.appendChild(card);
+        }
+
+    }
+
+    addContent();
 });
